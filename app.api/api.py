ffa08a710eb34a8af66d7cb56058ba2b133e426f
rabbitism <popmessiah@hotmail.com>
2019-08-05 22:18:22 +0800

    modify api

--- app.api/api.py
+++ app.api/api.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/", methods=['GET'] )
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
