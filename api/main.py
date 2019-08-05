169edc4d267486793dd5604543ef83cc22410b2f
rabbitism <popmessiah@hotmail.com>
2019-08-05 23:30:18 +0800

    test new api

--- api/main.py
+++ api/main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/cuisines", methods=['GET'])
+def return_cuisines():
+    return ["Chinese", "Indian", "American"]
+
 if __name__ == "__main__":
     app.run()
