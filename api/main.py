4fc0c035a58b7478631ae0d12aa09eebddc7be8d
rabbitism <popmessiah@hotmail.com>
2019-08-07 14:37:49 +0800

    support cross domain

--- api/main.py
+++ api/main.py
@@ -4,7 +4,20 @@ import os
 
 app = Flask(__name__)
 
-import os
+from functools import wraps
+from flask import make_response
+
+
+def allow_cross_domain(fun):
+    @wraps(fun)
+    def wrapper_fun(*args, **kwargs):
+        rst = make_response(fun(*args, **kwargs))
+        rst.headers['Access-Control-Allow-Origin'] = '*'
+        rst.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+        allow_headers = "Referer,Accept,Origin,User-Agent"
+        rst.headers['Access-Control-Allow-Headers'] = allow_headers
+        return rst
+    return wrapper_fun
 
 cuisines = []
 
