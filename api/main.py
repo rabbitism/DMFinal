60ea10d803de32b509fe0a2d4d48841ad09e96a0
rabbitism <popmessiah@hotmail.com>
2019-08-07 12:13:56 +0800

    update dependency

--- api/main.py
+++ api/main.py
@@ -1,9 +1,34 @@
 from flask import Flask, request, redirect
 import json
 import os
-import info as info
 
 app = Flask(__name__)
+
+import os
+
+cuisines = []
+
+with open(os.sep.join(['.', 'category_list.txt']), 'r') as f:
+    for line in f.readlines():
+        cuisines.append(line.replace('\n', ''))
+            
+top_list = {}
+
+with open(os.sep.join(['.', 'Task7Matrix.csv']), 'r') as mf:
+    content = mf.readlines()
+    for i in range(len(cuisines)):
+        rank = {}
+        scores = content[i].split(',')
+        top = []
+        for j in range(len(cuisines)):
+            if (i == j):
+                continue
+            rank[cuisines[j]] = (float)(scores[j])
+        sorted_rank = sorted(rank.items(), key = lambda x: x[1], reverse= True )
+        #print(cuisines[i], sorted_rank[0:5])
+        for item in sorted_rank:
+            top.append(item[0])
+        top_list[cuisines[i]] = top
 
 @app.route("/", methods=['GET'] )
 def hello():
@@ -11,12 +36,12 @@ def hello():
 
 @app.route("/cuisines", methods=['GET'])
 def return_cuisines():
-    return json.dumps(info.cuisines)
+    return json.dumps(cuisines)
 
 @app.route("/cuisine/<cuisine>/<top>", methods=['GET'])
 def return_top(cuisine: str, top: int):
     top_index = min(int(top), 40)
-    return json.dumps(info.top_list[cuisine][0:top_index])
+    return json.dumps(top_list[cuisine][0:top_index])
 
 
 if __name__ == "__main__":
